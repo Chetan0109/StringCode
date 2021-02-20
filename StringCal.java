a6ec897249116b632bc16923bb5aafe0c60e0d4d
Chetan0109 <motwanichetan9@gmail.com>
2021-02-20 16:36:44 +0530

    Third Commit, added new method

--- StringCal.java
+++ StringCal.java
@@ -5,11 +5,20 @@ public class StringCal{
 	public static void main(String args[]){
 		ReturnZeroIfNull();
 		ReturnNumbersIfNotNull();
+		ReturnSumIfDelimByComma();
 	}
 	
 	public static int add(String str){
 		if(str == ""){
 			return 0;
+		}
+		else if(str.contains(",")){
+			String[] numbers = str.split(",");
+			int j = 0;
+			for(int i = 0;i<numbers.length;i++){
+				j = j + Integer.parseInt(numbers[i]);
+			}
+			return j;
 		}
 		else{
 			return Integer.parseInt(str);
@@ -27,6 +36,13 @@ public class StringCal{
 	public static void ReturnNumbersIfNotNull(){
 		int sum = 12;
 		int val = StringCal.add("12");
+		compare(sum,val);
+	}
+	public static void ReturnSumIfDelimByComma(){
+		int sum = 6;
+		int val;
+		String s = "1,2,3";
+		val = add(s);
 		compare(sum,val);
 	}
 	public static void compare(int sum, int val){
